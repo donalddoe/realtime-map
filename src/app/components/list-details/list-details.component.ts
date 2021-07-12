1d399c27ed36832daddc2a3665e482ea418e0849
Donalddoe <Donald.Seanedzu@azubiafrica.org>
2021-07-12 08:41:25 +0000

    styling the map-list-details

--- src/app/components/list-details/list-details.component.ts
+++ src/app/components/list-details/list-details.component.ts
@@ -23,5 +23,9 @@ export class ListDetailsComponent implements OnInit {
       })
   }
 
+  highAmenitiesIcons = ['assets/images/BlackStainless-Appliances.svg', 'assets/images/Granite-Countertops.svg', 'assets/images/BlackStainless-Appliances.svg', 'assets/images/DogWalk-Park.svg', 'assets/images/Jogging-Trail.svg', 'assets/images/Fitness-Center.svg', 'assets/images/Vallet-Trash.svg'];
 
+  communityFeaturesIcon = ['assets/images/Swimming+Pool.svg', 'assets/images/Picnic+Area.svg', 'assets/images/School+Bus+Pickup.svg', 'assets/images/Shuttle+Route.svg', 'assets/images/Business+Center.svg', 'assets/images/Laundry+Rooms.svg']
+
+  unitOptionsIcon = ['assets/images/Microwave.svg', 'assets/images/Dishwasher.svg', 'assets/images/Pantry.svg', 'assets/images/Refrigerator.svg', 'assets/images/Walk-in+Closet.svg', 'assets/images/Patio.svg', 'assets/images/Fireplace.svg', 'assets/images/Berber+Carpet.svg', 'assets/images/Ceiling+Fans.svg', 'assets/images/Hot+TubJacuzzi.svg']
 }
